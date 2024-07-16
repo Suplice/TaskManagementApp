4c329c10fe9b40e653e080abacb4a898e73f708d
Mateusz Duma <mateuszsuplice@gmail.com>
2024-07-16 16:07:50 +0200

    Implemented Login Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import Navigationbar from "./Components/NavigationBar/Navigationbar";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TaskList from "./Components/TaskList/TaskList";
+import Login from "./Components/Login/Login";
 import "./App.css";
 
 function App() {
   return (
-      <Router>
-          <Navigationbar></Navigationbar>
-          <Routes>
-              <Route path="/TaskList" element={<TaskList/>} ></Route>
-              <Route path="/index" ></Route>
-          </Routes>
-      </Router>
+    <Router>
+      <Navigationbar></Navigationbar>
+      <Routes>
+        <Route path="/taskList" element={<TaskList />}></Route>
+        <Route path="/login" element={ <Login /> }></Route>
+      </Routes>
+    </Router>
   );
 }
 
