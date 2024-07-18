da8a0ea29898445f66fda4124c0544a31202475c
Mateusz Duma <mateuszsuplice@gmail.com>
2024-07-18 18:50:16 +0200

    Same as last commit

--- TaskManagementApp.Core/ApiResponse/ApiResponse.cs
+++ TaskManagementApp.Core/ApiResponse/ApiResponse.cs
@@ -13,12 +13,15 @@ namespace TaskManagementApp.Core.ApiResponse
         public string message { get; set; }
         public T data { get; set; }
         public IDictionary<string , List<string>?>? errors { get; set; }
+        public string JwtToken { get; set; }
 
-        public ApiResponse(bool _success, string _message, T _data, IDictionary<string, List<string>?>? _errors = null) { 
+        public ApiResponse(bool _success, string _message, T _data, IDictionary<string, List<string>?>? _errors = null, string _JwtToken="") { 
             success = _success;
             message = _message;
             data = _data;
             errors = _errors;
+            JwtToken = _JwtToken;
         }
+
     }
 }
---
+++ TaskManagementApp.Core/JwtSettings/JwtSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagementApp.Core.JwtSettings
+{
+    public class JwtSettings
+    {
+        public string Issuer {  get; set; }
+        public string Audience { get; set; }
+        public string SecretKey { get; set; }
+        public int ExpiryHours { get; set; }
+
+    }
+}
