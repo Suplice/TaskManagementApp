9c33bfe7a23cb1c8d6fff95dd545003de89cdd3d
Mateusz Duma <mateuszsuplice@gmail.com>
2024-07-19 14:09:09 +0200

    All tasks now have correct data about them

--- frontend/src/Components/Task/Task.jsx
+++ frontend/src/Components/Task/Task.jsx
@@ -1,14 +1,26 @@
 import "./Task.css";
 
-function Task() {
+function Task({ title, description, isCompleted, startDate, dueDate }) {
+
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth()).padStart(2, '0');
+        const day = String(date.getDay()).padStart(2, '0');
+        const hour = String(date.getHours()).padStart(2, '0');
+        const minute = String(date.getMinutes()).padStart(2, '0');
+
+        return `${year}-${month}-${day} ${hour}:${minute}`;
+    };
+
   return (
     <div className="Task-container">
       <div className="TaskInformation">
-        <div className="TaskTitle">Title</div>
-        <div className="TaskDescription">Description</div>
-        <div className="TaskStartTime">Start Time: </div>
-        <div className="TaskEndTime">End Time: </div>
-        <div className="TaskCompletionStatus">Completed: </div>
+              <div className="TaskTitle">Title: {title}</div>
+              <div className="TaskDescription">Description: {description} </div>
+              <div className="TaskStartTime">Start Time: {formatDate(startDate)}</div>
+              <div className="TaskEndTime">End Time: {formatDate(dueDate)}</div>
+              <div className="TaskCompletionStatus">Completed: { isCompleted ? "yes" : "no"}</div>
       </div>
       <div className="TaskButtons">
         <button className="CompleteTaskButton"> Complete </button>
