d6ab9cd5f744c6cfc4974dac507858903134edb5
Mateusz Duma <mateuszsuplice@gmail.com>
2024-07-22 15:13:05 +0200

    Implemented new DTO to transfer data when adding task

---
+++ TaskManagementApp.DTO/DTOs/AddTaskRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApp.DTO.DTOAttributes;
+
+namespace TaskManagementApp.DTO.DTOs
+{
+    public class AddTaskRequest
+    {
+        [Required]
+        public string title { get; set; }
+
+        [Required]
+        public string description { get; set; }
+
+        [Required]
+        [DateGreaterThanToday]
+        public DateTime dueDate { get; set; }
+    }
+}
