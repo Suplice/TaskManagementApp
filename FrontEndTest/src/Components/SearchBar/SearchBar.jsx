ec3f22578b1e9050066ddbbf6f31f33f35c49aad
Mateusz Duma <mateuszsuplice@gmail.com>
2024-08-29 17:40:28 +0200

    Search bar can now be cleared using the X button

--- FrontEndTest/src/Components/SearchBar/SearchBar.jsx
+++ FrontEndTest/src/Components/SearchBar/SearchBar.jsx
@@ -1,19 +1,39 @@
 import './SearchBar.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function SearchBar({ onSearch }) {
 
     const [placeholder, setPlaceholder] = useState('Search...');
     const [text, setText] = useState('');
+    const [removeIconVisibility, setRemoveIconVisibility] = useState(false);
+
+    useEffect(() => {
+        function ChangeIconVisibility() {
+            if (text === '') {
+                setRemoveIconVisibility(true);
+            }
+            else {
+                setRemoveIconVisibility(false);
+            }
+        }
+
+        ChangeIconVisibility();
+    }, [text]);
 
     const handleSearchClick = () => {
         onSearch(text);
     }
 
+    const handleRemoveIconClick = () => {
+        setText('');
+        onSearch('');
+    };
+
 
     return (<div id="SearchBarBox">
-        <input id="SearchText" value={text}  onChange={(event) => { setText(event.target.value) }} placeholder={placeholder} onBlur={() => { setPlaceholder('Search...') }} onFocus={() => { setPlaceholder('') }}></input>
-        <img src='/public/SearchIcon.png' onClick={ handleSearchClick }></img>
+        <input id="SearchText" value={text} onChange={(event) => { setText(event.target.value) }} placeholder={placeholder} onBlur={() => { setPlaceholder('Search...') }} onFocus={() => { setPlaceholder('') }}></input>
+        <img src='/public/removeSearchIcon.jpg' id='removeIcon' hidden={removeIconVisibility} onClick={handleRemoveIconClick }></img>
+        <img src='/public/SearchIcon.png' id='searchIcon' onClick={handleSearchClick}></img>
     </div>);
 }
 
